c1599983e891f3156366f75ee8fed8aea9c5bf78
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-18 19:14:18 -0300

    theme

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,22 @@
+import { StatusBar, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { NativeBaseProvider, Box } from "native-base";
+
+import { THEME } from './src/theme';
+
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
-      <Box>Hello world</Box>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
