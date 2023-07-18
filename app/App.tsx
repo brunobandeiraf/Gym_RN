9e89105c34de6982b592f50063cf3a80f3e8d42d
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-18 18:45:33 -0300

    native base

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,12 @@
-import { Text, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { NativeBaseProvider, Box } from "native-base";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      {fontsLoaded ? <Text>Hello World</Text> : <View />}
-    </View>
+    <NativeBaseProvider>
+      <Box>Hello world</Box>
+    </NativeBaseProvider>
   );
 }
--- app/README.md
+++ app/README.md
@@ -6,3 +6,8 @@ npm install --save-dev babel-plugin-module-resolver
 
 # Font Google - Roboto
 npx expo install expo-font @expo-google-fonts/roboto
+
+# NativeBase
+- npm install native-base
+- npx expo install react-native-svg@12.1.1
+- npx expo install react-native-safe-area-context@3.3.2
