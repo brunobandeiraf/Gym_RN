5e504884602623b73a78a1cb7cd5978ca9cafddb
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-18 22:32:02 -0300

    focus

--- app/src/components/Input.tsx
+++ app/src/components/Input.tsx
@@ -1,18 +1,24 @@
 import { Input as NativeBaseInput, IInputProps } from 'native-base';
 
+//IInputProps habilita o { ...rest}
 export function Input({ ...rest }: IInputProps) {
   return (
     <NativeBaseInput 
-      bg="gray.700"
-      h={14}
-      px={4}
+      bg="gray.700" //background
+      h={14} //altura
+      px={4} //padding
       borderWidth={0}
-      fontSize="md"
+      fontSize="md" //maior
       color="white"
-      fontFamily="body"
-      mb={4}
+      fontFamily="body" //mesma font do body
+      mb={4} //margin-bottom
       placeholderTextColor="gray.300"
-      {...rest}
+      _focus={{
+        bgColor: 'gray.700',
+        borderWidth: 1,
+        borderColor: 'green.500'
+      }}
+      {...rest} 
     />
   );
 }
