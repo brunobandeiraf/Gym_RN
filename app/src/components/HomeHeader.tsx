f7d8eab1125ed4b4f54bf820402f0d2f48acd7df
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-19 17:01:48 -0300

    homeheader

--- app/src/components/HomeHeader.tsx
+++ app/src/components/HomeHeader.tsx
@@ -4,13 +4,13 @@ export function HomeHeader() {
   return (
     // HStack posiciona na horizontal
     // VStack posiciona na vertical
-    <HStack> 
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100">
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
-        <Heading color="gray.100">
+        <Heading color="gray.100" fontSize="md">
           Bruno
         </Heading>
       </VStack>
