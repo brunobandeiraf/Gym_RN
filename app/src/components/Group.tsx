e94295e084b2f33728244129fe688d573914260e
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-19 17:23:51 -0300

    home

--- app/src/components/Group.tsx
+++ app/src/components/Group.tsx
@@ -1,18 +1,32 @@
-import { Text } from 'native-base';
+import { Text, Pressable, IPressableProps } from 'native-base';
+// Pressable - região de clique
 
-type Props = {
+type Props  = IPressableProps & {
   name: string;
 }
 
 export function Group({ name }: Props) {
   return (
-    <Text
-      color="gray.200"
-      textTransform="uppercase"
-      fontSize="xs"
-      fontWeight="bold"
-    >
-      {name}
-    </Text>
+      
+      <Pressable
+          mr={3}
+          w={24}
+          h={10}
+          bg="gray.600"
+          rounded="md" //cantos arredondados
+          justifyContent="center"
+          alignItems="center"
+          overflow="hidden" // manter dentro dos limites do botão
+          {...rest}
+      >
+        <Text
+          color="gray.200"
+          textTransform="uppercase"
+          fontSize="xs"
+          fontWeight="bold"
+        >
+          {name}
+        </Text>
+    </Pressable>
   );
 }
