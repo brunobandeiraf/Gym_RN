94b521b6fcdb7b01264e3747d58ebc3f4586053a
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-18 22:53:18 -0300

    finaliznado signin

--- app/src/components/Button.tsx
+++ app/src/components/Button.tsx
@@ -3,21 +3,27 @@ import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base';
 // tipagem do tipo botton
 type Props = IButtonProps & {
   title: string;
+  variant?: 'solid' | 'outline';
 }
+// ? é opcinal
+
 //{ title, ...rest } são as propriedades do botão
-export function Button({ title, variant, ...rest }: Props) {
+export function Button({ title, variant = 'solid', ...rest }: Props) {
   return (
     <ButtonNativeBase
         w="full" //width
         h={14}  //height
 
         bg={variant === 'outline' ? 'transparent' : 'green.700'}
+        // Se for outline terá a bordar transparent
+        // se não, será green.700
+
         borderWidth={variant === 'outline' ? 1 : 0}
-        borderColor="green.500"
+        borderColor="green.500" // cor da borda
 
         rounded="sm" //cantos arrendodados
         
-        _pressed={{ //quando estiver precioando
+        _pressed={{ //quando estiver precionando
             bg: variant === 'outline' ? 'gray.500' : 'green.500'  
         }}
 
--- app/src/screens/SignIn.tsx
+++ app/src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, Heading } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -8,44 +8,56 @@ import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700" px={10}>
-      {/* px padding
-      bg é background */}
-        <Image 
-            source={BackgroundImg}
-            alt="Pessoas treinando"
-            resizeMode="contain"
-            position="absolute"
-        />
-        {/* my é posicionamento na vertical */}
-        <Center my={24}>
-            <LogoSvg />
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
 
-            <Text color="gray.100" fontSize="sm">
-                Treine sua mente e o seu corpo.
-            </Text>
+      <VStack flex={1} bg="gray.700" px={10}>
+        {/* px padding
+        bg é background */}
+          <Image 
+              source={BackgroundImg}
+              alt="Pessoas treinando"
+              resizeMode="contain"
+              position="absolute"
+          />
+          {/* my é posicionamento na vertical */}
+          <Center my={24}>
+              <LogoSvg />
+
+              <Text color="gray.100" fontSize="sm">
+                  Treine sua mente e o seu corpo.
+              </Text>
+          </Center>
+          {/* mb é margin-bottom */}
+          <Center>
+              <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+                  Acesse a conta
+              </Heading>
+
+              <Input 
+                placeholder="E-mail" 
+                keyboardType="email-address"
+                autoCapitalize="none" //tudo em minísculo
+
+              />
+              <Input 
+                placeholder="Senha" 
+                secureTextEntry //input de senha
+              />
+
+              <Button title="Acessar" />
         </Center>
-        {/* mb é margin-bottom */}
-        <Center>
-            <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-                Acesse a conta
-            </Heading>
 
-            <Input 
-              placeholder="E-mail" 
-              keyboardType="email-address"
-               autoCapitalize="none" //tudo em minísculo
+        <Center mt={24}> {/* mt - margin-top, mb - margin-bottom*/}
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
+        </Center>
 
-            />
-            <Input 
-              placeholder="Senha" 
-              secureTextEntry //input de senha
-            />
-
-            <Button title="Acessar" />
-      </Center>
-
-      <Button title="Criar Conta" variant="outline" />
-    </VStack>
+        <Button 
+          title="Criar Conta" 
+          variant="outline" 
+        />
+      </VStack>
+    </ScrollView>
   );
 }
