2632fc19a7bc4a20e71393d097875dc04936f68d
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-20 20:41:13 -0300

    historycard

--- app/src/components/HistoryCard.tsx
+++ app/src/components/HistoryCard.tsx
@@ -2,7 +2,10 @@ import { Heading, HStack, Text, VStack } from 'native-base';
 
 export function HistoryCard() {
   return (
-    <HStack w="full" px={5} py={4} mb={3} bg="gray.600" rounded="md" alignItems="center" justifyContent="space-between">
+    <HStack w="full" px={5} py={4} mb={3} 
+    bg="gray.600" rounded="md" 
+    alignItems="center" 
+    justifyContent="space-between">
       <VStack mr={5}>
         <Heading color="white" fontSize="md" textTransform="capitalize">
           Costas
