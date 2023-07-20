028ea1b23b19f161ebe6d600ff80cff7604782f4
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-20 13:32:24 -0300

    group

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { HStack, VStack } from 'native-base';
+import { FlatList, HStack, VStack } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
@@ -6,24 +6,32 @@ import { useState } from 'react';
 
 export function Home() {
 
-  const [groupSelected, setGroupSelected] = useState('costa')
+  const [groupSelected, setGroupSelected] = useState('Costas')
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
 
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-        <HStack>
+      <FlatList 
+        data={groups} //Nome do grupo será a chave
+        keyExtractor={item => item} // Desestrutura os itens
+        renderItem={({ item }) => ( // Percorre um a um dos itens
           <Group 
-            name="costa" 
-            isActive={groupSelected === 'costa'} // Verifica se está ativo
-            onPress={() => setGroupSelected('costa')}
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
           />
-          <Group 
-            name="ombro" 
-            isActive={groupSelected === 'ombro'}
-            onPress={() => setGroupSelected('ombro')}
-          />
-        </HStack>
+        )}
+
+        horizontal // FlatList na horizontal
+        showsHorizontalScrollIndicator={false} // Remover barra de scroll
+        _contentContainerStyle={{
+          px: 8, //padding
+        }}
+        my={10} // margin vertical 
+        maxH={10} // Máximo da altura será 10, senão oculta espaço de toda tela
+      />
 
     </VStack>
   );
