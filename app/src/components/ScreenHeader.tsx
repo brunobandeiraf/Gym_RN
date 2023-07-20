2c8a3b49f6e4674b7dfb187b706905eef638f8f6
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-20 20:24:25 -0300

    historycard

--- app/src/components/ScreenHeader.tsx
+++ app/src/components/ScreenHeader.tsx
@@ -6,6 +6,7 @@ type Props = {
 
 export function ScreenHeader({ title }: Props) {
   return (
+    // pb padding-bottom e pt padding-top
     <Center bg="gray.600" pb={6} pt={16}>
       <Heading color="gray.100" fontSize="xl">
         {title}
