0e1ae333ef9fd59a67c20ce929b9f0ed4a565610
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-20 20:27:36 -0300

    historycard

--- app/src/screens/History.tsx
+++ app/src/screens/History.tsx
@@ -1,10 +1,14 @@
-import { ScreenHeader } from '@components/ScreenHeader';
 import { VStack } from 'native-base';
+
+import { HistoryCard } from '@components/HistoryCard';
+import { ScreenHeader } from '@components/ScreenHeader';
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico' />
+
+      
     </VStack>
   );
 }
