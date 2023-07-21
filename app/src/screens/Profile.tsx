d6c8049b544baa23b6c159794f33136a9be00f36
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-20 21:20:54 -0300

    profile

--- app/src/screens/Profile.tsx
+++ app/src/screens/Profile.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
-import { Center, ScrollView, VStack, Skeleton } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 
 const PHOTO_SIZE = 33;
 
@@ -34,7 +37,52 @@ export function Profile() {
                 size={PHOTO_SIZE}
               />
           }
+
+          <TouchableOpacity>
+            <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input 
+            bg="gray.600" 
+            placeholder='Nome' 
+          />
+
+          <Input 
+            bg="gray.600" 
+            placeholder="E-mail"
+            isDisabled
+          />
+
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input 
+            bg="gray.600"
+            placeholder="Senha antiga"
+            secureTextEntry //ocultar a senha do usuário
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="Nova senha"
+            secureTextEntry
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button title="Atualizar" mt={4} />
+        </VStack>
+
       </ScrollView>
     </VStack>
   );
